f0a7bf2b0df48d47c56af100a77219725a77a014
zhengwei <zhengwei1949@qq.com>
2017-12-25 21:28:16 +0800

    a

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,21 @@
 //开始享受写代码的旅程吧~~
+var vm = new Vue({
+    el:'#app',
+    data:{
+
+    },
+    filters:{
+
+    },
+    mounted:function(){
+        this.cartView();
+    },
+    methods:{
+        cartView:function(){
+            var that = this;
+            this.$http.get('data/cart.json').then(function(res){
+                console.log(res);
+            })
+        }
+    }
+})

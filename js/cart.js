61327faf18fe5449f65a9b83629e7b91c1291a85
zhengwei <zhengwei1949@qq.com>
2017-12-25 22:12:56 +0800

    a

--- js/cart.js
+++ js/cart.js
@@ -2,20 +2,45 @@
 var vm = new Vue({
     el:'#app',
     data:{
-
+        totalMoney:0,
+        productList:[]
     },
     filters:{
-
+        formatMoney:function(v){
+            return '￥'+v.toFixed(2);
+        }
     },
     mounted:function(){
-        this.cartView();
+        this.$nextTick(function(){
+            vm.cartView();
+        })
     },
     methods:{
         cartView:function(){
             var that = this;
             this.$http.get('data/cart.json').then(function(res){
-                console.log(res);
+                // console.log(res);
+                that.totalMoney = res.data.result.totalMoney;
+                that.productList = res.data.result.list;
             })
+        },
+        changeMoney:function(item,status){
+            if(status === 1){
+                item.count++;
+            }else{
+                item.count--;
+            }
+        },
+        selectProduct:function(item){
+            if(!item.checked){
+                Vue.set(item, 'checked', true);
+            }else{
+                Vue.set(item, 'checked', false);
+            }
         }
     }
+})
+
+Vue.filter('money',function(v,status){
+    return '￥'+v.toFixed(2) + status;
 })
